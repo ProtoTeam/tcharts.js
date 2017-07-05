77550b062c033ddcb45a6cf311a208ce2c55f24e
hustcc <xiaowei.wzw@antfin.com>
2017-07-05 19:44:32 +0800

    add props align for Text, default is center

--- src/core/Text.js
+++ src/core/Text.js
@@ -15,7 +15,7 @@ const Element = require('./Element');
  * hello world，这个是一个控制台图标库
  */
 class Text extends Element {
-  constructor(p, text) {
+  constructor(p, text, align = 'center') {
     super();
     invariant(
       types.isPoint(p) && types.isString(text),
@@ -23,17 +23,44 @@ class Text extends Element {
       types.typeof(p),
       types.typeof(text));
 
+    invariant(
+      ['center', 'left', 'right'].indexOf(align) >= 0,
+      'TCharts: constructor props `align` of Text should be one of [\'center\', \'left\', \'right\'], got %s.',
+      align);
     this.p = p;
     this.text = text;
+    this.align = align;
 
-    // 居中显示
-    const _width = evenly(WordWidth(this.text), 2, 0);
-    this.box = {
-      x1: p.x - _width[0] + 1, // 因为当前 p 位置还可以存一个字符串
-      y1: p.y,
-      x2: p.x + _width[1],
-      y2: p.y,
-    };
+
+    const textWidth = WordWidth(this.text);
+
+    if (this.align === 'center') {
+      // 居中显示
+      const _width = evenly(textWidth, 2, 0);
+      this.box = {
+        x1: p.x - _width[0] + 1, // 因为当前 p 位置还可以存一个字符串
+        y1: p.y,
+        x2: p.x + _width[1],
+        y2: p.y,
+      };
+    } else if (this.align === 'left') {
+      // 局左显示
+      this.box = {
+        x1: p.x,
+        y1: p.y,
+        x2: p.x + textWidth + 1,
+        y2: p.y,
+      };
+    } else {
+      // 居右显示
+      this.box = {
+        x1: p.x - textWidth + 1,
+        y1: p.y,
+        x2: p.x,
+        y2: p.y,
+      };
+    }
+
 
     this.initLayer();
   }
